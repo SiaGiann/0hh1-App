b1d5df42c66edde51ba8392feb7c9b7bf268b56b
SiaGiann <siayiannaki@gmail.com>
2018-03-11 12:59:03 +0100

    new game endpoint

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const usersRouter = require('./users/router')
 const authRouter = require('./authentication/router')
 const tokenMiddleware = require('./authentication/middleware').tokenMiddleware
 const requireUser = require('./authentication/middleware').requireUser
+const { Game } = require('./models')
 
 const port = process.env.PORT || 4001
 
@@ -20,6 +21,41 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
   next()
+})
+
+app.post('/games', (req, res) => {
+  const initialBoard = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+  ]
+  const gameStatus = "NEW"
+  const { user_id } = req.body
+
+  const game = {
+    user_id: user_id,
+    board_status: JSON.stringify(initialBoard),
+    game_status: gameStatus
+  }
+
+  Game
+    .create(game)
+    .then(entity => {
+      res.status(201)
+      res.json({
+        board_status: entity.board_status,
+        game_status: entity.game_status
+      })
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).send({
+        message: 'Something went wrong'
+      })
+    })
 })
 
 app.listen(port, () => {
--- server/migrations/20180311110316-create-game.js
+++ server/migrations/20180311110316-create-game.js
@@ -9,6 +9,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       user_id: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       board_status: {
